694e0c57c2ccdf846c1413196b02ea282d69ca13
Sahib Arora <sahibarora1997@gmail.com>
2017-10-10 11:20:56 -0400

    Add files via upload

---
+++ Hello_World/hello.java
@@ -0,0 +1,10 @@
+class hello                                        // In java we always save file with the name of class which contains main function
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!!!");         // it is a function in java which returns stuff onto the screen, here it is returning "hello World to the screen"
+	}
+	          // here public because we want main to be accessible by all the classes
+			  // static because here main belongs to the entire class
+			  // void because its not going to return any value
+}
